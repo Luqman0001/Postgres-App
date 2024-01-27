65e259206866458f6f7d35ac0151c1bcf3b3ec6e
Luqman0001 <luqman@codek.tech>
2024-01-27 22:36:09 +0500

    added index.ts on every entity

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { Express } from 'express-serve-static-core';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const server = express();
+async function bootstrap(expressInstance: Express) {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+    { cors: true },
+  );
+  app.enableCors();
+  return app.init();
 }
-bootstrap();
+
+async function start() {
+  try {
+    await bootstrap(server);
+    return server;
+  } catch (e) {
+    Logger.error(e);
+    process.exit(1);
+  }
+}
+(async () => {
+  await start();
+  let PORT = process.env.PORT || 3001
+  server.listen(PORT);
+  Logger.debug(`Server is now running on port ${PORT}`)
+})();
